a5fcc96872ecca1c46e20ba15cfe947a88a532c4
muyang <muyang@zenki.ai>
2020-09-11 14:42:07 +0800

    堆

--- muyang/my-test/树/level_order.js
+++ muyang/my-test/树/level_order.js
@@ -11,64 +11,64 @@ var root_node = tree.root;
 // D  E  F
 // G
 function level_order(node) {
-    var queue = new Queue_1["default"]();
-    queue.push(node);
-    queue.push(0);
-    var line = '';
-    while (true) {
-        var cur_node = queue.shift();
-        if (cur_node === 0) {
-            console.log(line);
-            line = '';
-            if (queue.isEmpty()) {
-                break;
-            }
-            queue.push(0);
-        }
-        else {
-            line += cur_node.data;
-            var left = cur_node.leftChild;
-            if (left) {
-                queue.push(left);
-            }
-            var right = cur_node.rightChild;
-            if (right) {
-                queue.push(right);
-            }
-        }
+  var queue = new Queue_1["default"]();
+  queue.push(node);
+  queue.push(0);
+  var line = '';
+  while (true) {
+    var cur_node = queue.shift();
+    if (cur_node === 0) {
+      console.log(line);
+      line = '';
+      if (queue.isEmpty()) {
+        break;
+      }
+      queue.push(0);
     }
+    else {
+      line += cur_node.data;
+      var left = cur_node.leftChild;
+      if (left) {
+        queue.push(left);
+      }
+      var right = cur_node.rightChild;
+      if (right) {
+        queue.push(right);
+      }
+    }
+  }
 }
 level_order(root_node);
 // 输出指定层的节点个数（困难模式+）
 // 实现函数get_width 返回第n层的节点个数
 function get_width(node, n) {
-    // 实现你的函数
-    var queue = new Queue_1["default"]();
-    var index = 0;
-    var size = 1;
-    queue.push(node);
-    queue.push(0);
-    while (true) {
-        var cur_node = queue.shift();
-        if (cur_node === 0) {
-            index += 1;
-            if (queue.isEmpty() || index === n) {
-                return size;
-            }
-            size = queue.size();
-            queue.push(0);
-        }
-        else {
-            var left = cur_node.leftChild;
-            if (left) {
-                queue.push(left);
-            }
-            var right = cur_node.rightChild;
-            if (right) {
-                queue.push(right);
-            }
-        }
+  // 实现你的函数
+  var queue = new Queue_1["default"]();
+  var index = 0;
+  var size = 1;
+  queue.push(node);
+  queue.push(0);
+  while (true) {
+    var cur_node = queue.shift();
+    if (cur_node === 0) {
+      index += 1;
+      if (queue.isEmpty() || index === n) {
+        return size;
+      }
+      size = queue.size();
+      queue.push(0);
     }
+    else {
+      var left = cur_node.leftChild;
+      if (left) {
+        queue.push(left);
+      }
+      var right = cur_node.rightChild;
+      if (right) {
+        queue.push(right);
+      }
+    }
+  }
 }
 console.log(get_width(root_node, 1));
 console.log(get_width(root_node, 2));
